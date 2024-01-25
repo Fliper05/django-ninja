6cd4c431131eb60e369f77bb3377f63ab1e68e19
Petr Rudolf Malecek <petrmalecek@systexo.com>
2024-01-25 23:15:51 +0100

    debugging api

--- tasks/api.py
+++ tasks/api.py
@@ -11,26 +11,25 @@ class TaskSchema(Schema):
     completed_at: datetime.datetime
     status: str
 
-@api.get("/tasks")
+@api.get("/tasks", response={200: list[TaskSchema]})
 def get_tasks(request):
     tasks = Task.objects.all()
-    return {"tasks": [TaskSchema.from_orm(task) for task in tasks]}
+    return [TaskSchema.from_orm(task) for task in tasks]
 
-@api.post("/tasks")
+@api.post("/tasks", response={201: TaskSchema})
 def create_task(request, task: TaskSchema):
     task_obj = Task.objects.create(**task.dict())
-    return {"task": TaskSchema.from_orm(task_obj)}
+    return TaskSchema.from_orm(task_obj)
 
-@api.put("/tasks/{task_id}")
+@api.put("/tasks/{task_id}", response={200: TaskSchema})
 def update_task(request, task_id: int, task: TaskSchema):
     task_obj = Task.objects.get(id=task_id)
     for attr, value in task.dict().items():
         setattr(task_obj, attr, value)
     task_obj.save()
-    return {"task": TaskSchema.from_orm(task_obj)}
+    return TaskSchema.from_orm(task_obj)
 
-
-@api.delete("/tasks/{task_id}")
+@api.delete("/tasks/{task_id}", response={200: {"message": str}})
 def delete_task(request, task_id: int):
     task_obj = Task.objects.get(id=task_id)
     task_obj.delete()
